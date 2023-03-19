b395d8a99447a85805c76ab2667a93dd74172d4b
Jacob Lin <jacoblindev@gmail.com>
2023-03-19 23:36:05 +0800

    init: project started...!

---
+++ src/main/java/dev/jacoblin/MainApplication.java
@@ -0,0 +1,12 @@
+package dev.jacoblin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
